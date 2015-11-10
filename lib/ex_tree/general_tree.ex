6ead8a89d412909168aa63f9649873d58f7ca5e2
Sébastien Varlet <sebastien.varlet@gmail.com>
2015-11-10 17:13:45 +0000

    Begin implementation of a GeneralTree type
    - Define of the GeneralTree type
    - Implement the leaf? function
    - Implement a depth-first traversal function

---
+++ lib/ex_tree/general_tree.ex
@@ -0,0 +1,29 @@
+defmodule ExTree.GeneralTree do
+  alias ExTree.GeneralTree
+
+  @type t :: %GeneralTree{value: any, children: list}
+  defstruct value: nil, children: nil
+
+  def leaf?(tree) do
+    case tree do
+      %GeneralTree{children: nil} -> true
+      %GeneralTree{children: []} -> true
+      _ -> false
+    end
+  end
+
+  @doc """
+  Depth first traversal of a tree.
+
+  Invokes node_visitor on each node/leaf and accumulates results in an
+  accumulator.
+  """
+  @spec depth_first_traversal(t, any, (t -> any)) :: list
+  def depth_first_traversal(tree, acc \\ [], node_visitor) do
+    if leaf?(tree) do
+      acc ++ [node_visitor.(tree)]
+    else
+      List.foldl(tree.children, acc ++ [node_visitor.(tree)], fn node, acc -> depth_first_traversal(node, acc, node_visitor) end)
+    end
+  end
+end
---
+++ test/ex_tree/general_tree_test.exs
@@ -0,0 +1,47 @@
+defmodule ExTree.GeneralTreeTest do
+  use ExUnit.Case
+
+  alias ExTree.GeneralTree
+  import ExTree.GeneralTree
+
+  test "has a nil value by default" do
+    tree = %GeneralTree{}
+    assert nil == tree.value
+  end
+
+  test "has no children by default" do
+    tree = %GeneralTree{}
+    assert nil == tree.children
+  end
+
+  test "a tree with no children is a leaf" do
+    assert leaf?(%GeneralTree{value: "root"})
+    assert leaf?(%GeneralTree{value: "root", children: []})
+  end
+
+  test "a tree with children is not a leaf" do
+    subtree = %GeneralTree{value: "subtree"}
+    tree = %GeneralTree{value: "root", children: [subtree]}
+    assert leaf?(tree) == false
+  end
+
+  test "depth first traversal" do
+    #     1
+    #    / \
+    #   2   3
+    #  / \
+    # 4   5
+    subtree4 = %GeneralTree{value: 4}
+    subtree5 = %GeneralTree{value: 5}
+    subtree2 = %GeneralTree{value: 2, children: [subtree4, subtree5]}
+    subtree3 = %GeneralTree{value: 3}
+    subtree1 = %GeneralTree{value: 1, children: [subtree2, subtree3]}
+
+    value_extractor = fn node -> node.value end
+
+    assert depth_first_traversal(subtree4, value_extractor) == [4]
+    assert depth_first_traversal(subtree2, value_extractor) == [2, 4, 5]
+    assert depth_first_traversal(subtree1, value_extractor) == [1, 2, 4, 5, 3]
+  end
+
+end
